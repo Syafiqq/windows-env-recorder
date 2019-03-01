8840fa1357c93111495c336c4e8243832f9dad01
syafiqq <id.muhammad.syafiq@gmail.com>
2019-03-01 09:43:11 +0700

    Add key transversal

--- Main/Program.cs
+++ Main/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using IniParser;
+using IniParser.Model;
 using Microsoft.Win32;
 
 namespace Main
@@ -14,14 +15,37 @@ namespace Main
             var data = parser.ReadFile("config.ini");
             var path = data["Config"]["Path"];
             var date = $"{DateTime.Now:yyyyMMddHHmmss}";
-            BackupUserEnvironment($@"{Path.Combine(path, $"user-env-{date}.reg")}");
-            BackupSystemEnvironment($@"{Path.Combine(path, $"system-env-{date}.reg")}");
+            //BackupUserEnvironment($@"{Path.Combine(path, $"user-env-{date}.reg")}");
+            TransverseUserEnvironment($@"{Path.Combine(path, $"user-env-{date}.reg")}");
+            //BackupSystemEnvironment($@"{Path.Combine(path, $"system-env-{date}.reg")}");
         }
 
         private static void BackupUserEnvironment(string filepath)
         {
             var path = $@"{Registry.CurrentUser.Name}\Environment";
             BackupRegistry(path, filepath);
+        }
+        
+        private static void TransverseUserEnvironment(string filepath)
+        {
+            void _TransverseUserEnvironment(RegistryKey _key, IniData _data)
+            {
+                if (_key == null) return;
+                Console.WriteLine(_key.Name);
+                foreach (var v in _key.GetValueNames())
+                {
+                    Console.WriteLine(v);
+                }
+                foreach (var v in _key.GetSubKeyNames())
+                {
+                    if(v == null) continue;
+                    var child = _key.OpenSubKey(v);
+                    _TransverseUserEnvironment(child, _data);
+                }
+            }
+            var data = new IniData();
+            var key = Registry.CurrentUser.OpenSubKey("Environment");
+            _TransverseUserEnvironment(key, data);
         }
 
         private static void BackupSystemEnvironment(string filepath)
